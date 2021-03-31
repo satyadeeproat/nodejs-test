8ec27457e5d171fe0f92a4b1a9fa02d3df3de0ee
Roat <satyadeep.roat@SW-LP09442.local>
2021-03-31 16:36:33 +0530

    f

--- src/main.js
+++ src/main.js
@@ -1,36 +1,41 @@
 require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = process.env.port || 8081;
-const { v4: uuidv4 } = require('uuid');
+const { ApolloServer } = require('apollo-server');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const Query = `
+  
+  type Book {
+    title: String
+    author: Author
+  }
+  type Query {
+    _empty: String
+    books: [Book]
+  }
+`;
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+const server = new ApolloServer({
+  typeDefs: Query,
+  resolvers
+});
 
-let restaurants = [{id: '1234', name: 'Biryani Blues'}];
 
-app.get('/restaurants', (req, res) => {
-  console.log('mrkjgnk');
-  res.send(restaurants)
-})
-
-app.get('/restaurants/:id', (req, res) => {
-  res.send(restaurants)
-})
-
-app.post('/restaurants', (req, res) => {
-  res.send(restaurants)
-})
-
-app.put('/restaurants/:id', (req, res) => {
-  res.send(restaurants)
-})
-
-app.delete('/restaurants/:id', (req, res) => {
-  res.send(restaurants)
-})
-
-app.listen(port, () => {
-  console.log(`REST API server listening at http://localhost:${port}`)
-})
+server.listen(process.env.port || 8081).then(() => {
+  console.log(`
+    API Gateway Server is running!
+    Listening on port 8080
+  `);
+});
